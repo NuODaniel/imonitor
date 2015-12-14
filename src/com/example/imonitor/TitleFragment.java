2d829f077959b914580295264e36d6a766b5f6cf
Daniel Chung <miji_nuo@hotmail.com>
2015-12-14 23:41:08 +0800

    modified the layout xml, optimize the UI。

--- src/com/example/imonitor/TitleFragment.java
+++ src/com/example/imonitor/TitleFragment.java
@@ -4,10 +4,8 @@ import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
 public class TitleFragment extends Fragment
 {
@@ -19,17 +17,6 @@ public class TitleFragment extends Fragment
 			Bundle savedInstanceState)
 	{
 		View view = inflater.inflate(R.layout.fragment_title, container, false);
-		mLeftMenu = (ImageButton) view.findViewById(R.id.id_title_left_btn);
-		mLeftMenu.setOnClickListener(new OnClickListener()
-		{
-			@Override
-			public void onClick(View v)
-			{
-				Toast.makeText(getActivity(),
-						"i am an ImageButton in TitleFragment ! ",
-						Toast.LENGTH_SHORT).show();
-			}
-		});
 		return view;
 	}
 }
